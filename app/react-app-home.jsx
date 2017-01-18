8c496d8b4a1bdd853daf7028782c40f19f2188e7
StreetCoder <proshimul@yahoo.com>
2017-01-18 14:42:11 +0600

    update latest article

--- app/react-app-home.jsx
+++ app/react-app-home.jsx
@@ -15,3 +15,78 @@
 //        document.getElementById('greeting-div')
 //    );
 //}, 2000);
+
+var LatestArticle = React.createClass({
+
+
+
+    getInitialState: function() {
+        return {
+            data: [], title: [], filename: [], formatted_created_on: []
+        }
+    },
+
+
+    componentDidMount: function() {
+        $.get("/api/latest-article", function(result) {
+
+            console.log(result);
+
+            this.setState({
+                data: result
+            });
+        }.bind(this));
+    },
+
+
+    render: function() {
+        return (
+            <div>
+                {this.state.data.map((latest, i) => <TableRow key = {i} data = {latest} />)}
+            </div>
+        );
+    }
+});
+
+class TableRow extends React.Component {
+    render() {
+        return (
+
+        //<ul>
+        //    <li>{this.props.data.title}</li>
+        //    <li>{this.props.data.nid}</li>
+        //    <li>{this.props.data.filename}</li>
+        //</ul>
+        //    <div className={"col-xs-12 col-md-4 popular-article"}>
+        //    </div>
+
+                <div className={"col-xs-12 col-md-4 popular-article"}>
+                    <div className={"card"}>
+                        <a href="#" className={"clickable-card"}></a>
+                        <div className={"card-article-image"}>
+                            <img src={"http://mytonic-revamp-staging.s3.amazonaws.com/revamp/s3fs-public/" + this.props.data.filename} />
+                                <div className={"category-banner"}>Lifestyle</div>
+                            </div>
+                            <div className={"card-article"}>
+                                <h3>{this.props.data.title}</h3>
+                                <div className={"clearfix"}>
+                                    <p className={"pull-left"}> 185 Views</p>
+                                    <p className={"articles-seen-by pull-left"}><i className={"fa fa-smile-o"}></i> 223</p>
+                                </div>
+                                <div className={"clearfix"}>
+                                    <p className={"pull-left"}>{this.props.data.formatted_created_on}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+        );
+    }
+}
+
+
+
+ReactDOM.render(
+    <LatestArticle />, document.getElementById('react-latest-articles')
+);
